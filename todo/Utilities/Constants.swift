2deff34a96a528d8611a70363a3bf9576d2d0d95
G3N <fengyuan.yap@gmail.com>
2022-07-14 22:20:37 +0900

    restructured default codes

---
+++ todo/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  todo
+//
+//  Created by Feng Yuan Yap on 2022/07/14.
+//
+
+import SwiftUI
+
+let itemFormatter: DateFormatter = {
+  let formatter = DateFormatter()
+  formatter.dateStyle = .short
+  formatter.timeStyle = .medium
+  return formatter
+}()
